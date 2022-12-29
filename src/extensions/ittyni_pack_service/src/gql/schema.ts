c117679247c82e47f61aa67f7c736440b0bfb056
mkhmalech <m.khmalech@gmail.com>
2022-12-29 14:56:05 +0100

    create pack service

--- src/extensions/ittyni_pack_service/src/gql/schema.ts
+++ src/extensions/ittyni_pack_service/src/gql/schema.ts
@@ -1,12 +1,47 @@
 import { buildSchema } from "graphql";
+
+// pack variables
+const _id = `_id : ID!`
+const title = `title:String`
+const description = `description:String`
+const overviews = `overviews:String`
+const procedures = `procedures : [String]`
+const price = `price:String`
+const currency = `currency:String`
+
+const labm = `
+    type Account {
+        name : String
+    }
+    type Labm {
+        account : Account
+    }
+`
+const user = `User { firstName: String lastName: String _id : ID}`
+const pack = `Pack {${title} ${description} ${procedures} ${price} ${currency}}`
+
 export const PackSchema = buildSchema(`
 
+    ${labm}
+    type ${user}
+    
+    type PackDetails {
+       ${title} ${description} ${procedures} 
+       ${price} ${currency}
+       createdBy : User
+       labm : Labm
+    }
+    
+    type ${pack}
+
+    input _${pack}
+
     type PackQuery {
-        readAllPacks : String
+        readAllPacks : [PackDetails]
     }
 
     type PackMutation {
-        createPackForLab(_id: String): String
+        createPackForLab(${_id}, pack: _Pack): String
     }
 
     schema {
