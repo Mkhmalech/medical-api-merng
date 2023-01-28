d4cab65cd27fc2e73448f2b13aebb28fa63f70be
mkhmalech <m.khmalech@gmail.com>
2023-01-28 10:15:11 +0100

    add extension to cabinet

--- src/extensions/ittyni_cabinet_api/src/module/cabinets.ts
+++ src/extensions/ittyni_cabinet_api/src/module/cabinets.ts
@@ -1,60 +1,66 @@
-import { Schema, model, Document } from "mongoose";
+import { Schema, model, Document, SchemaTypes } from "mongoose";
 
 /**
- * labo extension gived to status by component
+ * cabinet extensions
  */
-const extensionSchema = new Schema({ 
-    componentName: String, create: Boolean, read: Boolean, update: Boolean, delete: Boolean 
+const extensionSchema = new Schema({
+    componentId: { type: Schema.Types.ObjectId, ref: "COMPONENTS" },
+    addedBy: {type: Schema.Types.ObjectId, ref: "USERS"},
+    createdAt : {type: String , default: new Date().toLocaleString()},
+    create: Boolean,
+    read: Boolean,
+    update: Boolean,
+    delete: Boolean
 })
 /**
  * payement
  */
 const payement = new Schema({
-    payementId : { type: Schema.Types.ObjectId, ref: 'PAYEMENT' }
+    payementId: { type: Schema.Types.ObjectId, ref: 'PAYEMENT' }
 })
 /**
  * waiting patient
  */
 const waitingPatient = new Schema({
-    patient : { type: Schema.Types.ObjectId, ref: 'PATIENT' },
-    arrivedAt : String,
-    finishedAt : String,
-    viewedAt : String,
-    number : Number,
-    icd : { type: Schema.Types.ObjectId, ref: 'ICD' },
-    motif : String,
-    visitType : String,
-    status : [{updatedAt : String, updatedBy : { type: Schema.Types.ObjectId, ref: 'USER' }, now : String, before: String}]
+    patient: { type: Schema.Types.ObjectId, ref: 'PATIENT' },
+    arrivedAt: String,
+    finishedAt: String,
+    viewedAt: String,
+    number: Number,
+    icd: { type: Schema.Types.ObjectId, ref: 'ICD' },
+    motif: String,
+    visitType: String,
+    status: [{ updatedAt: String, updatedBy: { type: Schema.Types.ObjectId, ref: 'USER' }, now: String, before: String }]
 })
 /**
  * patient of cabinet
  */
 const patient = new Schema({
-    patientId : { type: Schema.Types.ObjectId, ref: 'PATIENT' }
+    patientId: { type: Schema.Types.ObjectId, ref: 'PATIENT' }
 })
 interface ICabinetModel extends Document {
     account: {
         name: string,
         code?: number,
-        type? : string,
-        start? : string,
+        type?: string,
+        start?: string,
     }
     contact: {
         tele: any[],
         address: any,
-        website : string
+        website: string
     }
     classification: any,
-    
-    extensions : any[],
 
-    patients : any[],
+    extensions: any[],
 
-    payement : any[]
+    patients: any[],
 
-    waitingRoom : any[]
+    payement: any[]
 
-    views : number
+    waitingRoom: any[]
+
+    views: number
 }
 
 const CabinetSchema: Schema = new Schema({
@@ -70,9 +76,9 @@ const CabinetSchema: Schema = new Schema({
             fix: [String],
 
             fax: [String],
-            
+
         },
-        website: {type : String},
+        website: { type: String },
         address: {
             region: {
                 type: String,
@@ -103,24 +109,24 @@ const CabinetSchema: Schema = new Schema({
      * patients not yet 
      * confirmed by ID
      */
-    patients : [patient],
+    patients: [patient],
     /**
      * cabinet incomes
      * payement
      */
-    payements : [payement],
+    payements: [payement],
     /**
      * waitng room
      */
-    waitingRoom : [waitingPatient],
+    waitingRoom: [waitingPatient],
     /**
      * number of views
      */
-    views : Number,
+    views: Number,
     /**
      * rating
      */
-    visitorsRating : Number
+    visitorsRating: Number
 });
 
 export const CABINET = model<ICabinetModel>("CABINET", CabinetSchema);
