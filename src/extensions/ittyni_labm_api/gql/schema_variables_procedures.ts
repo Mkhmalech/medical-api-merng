04de2d6fd656ca6331ea7b9a52930a7e2331418d
khmammed <khmammed@gmail.com>
2024-03-09 11:12:14 +0100

    rewrite a procedure schema

---
+++ src/extensions/ittyni_labm_api/gql/schema_variables_procedures.ts
@@ -0,0 +1,23 @@
+// procedure names variables
+const Name_value = 'name_value: String';
+const Name_lang = 'name_lang: String';
+const name_country = 'name_country: String';
+const Name = `Name{${Name_value} ${Name_lang} ${name_country}}`
+
+// lab procedure codifications
+const Code_name = 'code_name: String';
+const Code_key = 'code_key: String';
+const Code_value = 'code_value: String';
+const Code_mnemonic = 'code_mnemonic: String';
+
+// lab procedure type
+const Type = 'type: String';
+
+// lab procedure components
+const Component = 'component: ID!';
+
+// lab procedure parameters
+const Parameter_name= 'parameter_name: String';
+const Parameter_mnemonic = 'parameter_mnemonic: String';
+const Parameter_unit = 'parameter_unit: String';
+const Parameter = `Parameter{${Parameter_name} ${Parameter_mnemonic} ${Parameter_unit}}`
